085ca60c3195202e3b6637bbe78aa14aab220643
Madhusudhan Anand <maddymaster@gmail.com>
2020-06-01 19:00:03 +0530

    added tests to create token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,7 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
-
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -52,3 +52,23 @@ class PublicUserApiTests(TestCase):
             email = payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """ Test that a token is created for the user """
+        payload = {'email': 'test@maddy.com', 'password': 'testpass'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """ Test that token is not created if invalid credentials are given """
+        create_user(email = 'test@maddy.com', password = "testpass")
+        payload = {'email': 'test@maddy.com', 'password':'wrong'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+         
