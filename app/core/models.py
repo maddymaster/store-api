385de38cd932cc9f697a5317fdddde8206947b36
Madhusudhan Anand <maddymaster@gmail.com>
2020-06-02 08:45:39 +0530

    fixed permissions error

--- app/core/models.py
+++ app/core/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         """ Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
-        user.is_supervisor = True
+        user.is_superuser = True
         user.save(using = self._db)
 
         return user
