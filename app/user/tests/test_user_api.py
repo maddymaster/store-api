1e79b15e6cd67a3dfd140611ae7560504b72afc7
Madhusudhan Anand <maddymaster@gmail.com>
2020-06-03 15:14:32 +0530

    Added endpoint for creating and authenticating users using Token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -83,6 +83,7 @@ class PublicUserApiTests(TestCase):
         """ Test that email and password are required """
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)    
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
 
          
